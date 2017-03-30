891c5fb6c712041f97716678facd71878536641b
Gurpreet Singh <gdhuper@gmail.com>
2017-03-29 19:40:09 -0700

    getting tiles with different zoom levels

--- js/app.js
+++ js/app.js
@@ -10,20 +10,75 @@ var	desc_text = document.getElementById("desc_text");
 //Adding event listener to button
 report_button.addEventListener('click', loadApi);
 
+/******Getting array of Tile ***********/
+
+function getTilesFromGeometry(geometry, template, zoom){
+  function long2tile(lon,zoom) {
+    return (Math.floor((lon+180)/360*Math.pow(2,zoom)));
+  }
+  function lat2tile(lat,zoom) {
+    return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom)));
+  }
+  function replaceInTemplate(point){
+    return template.replace('{z}', point.z)
+      .replace('{x}', point.x)
+      .replace('{y}', point.y);
+  }
+
+  var allLat = geometry.map(function(point){
+    return point.lat;
+  });
+  var allLng = geometry.map(function(point){
+    return point.lng;
+  });
+  var minLat = Math.min.apply(null, allLat);
+  var maxLat = Math.max.apply(null, allLat);
+  var minLng = Math.min.apply(null, allLng);
+  var maxLng = Math.max.apply(null, allLng);
+  var top_tile    = lat2tile(maxLat, zoom); // eg.lat2tile(34.422, 9);
+  var left_tile   = long2tile(minLng, zoom);
+  var bottom_tile = lat2tile(minLat, zoom);
+  var right_tile  = long2tile(maxLng, zoom);
+
+  var tiles = [];
+  for (var y = top_tile; y < bottom_tile + 1; y++) {
+    for (var x = left_tile; x < right_tile + 1; x++) {
+      tiles.push(replaceInTemplate({x, y, z: zoom}))
+    }
+  }
+
+  return tiles;
+}
+
+
+
+
+/*****************************************/
+
+
+
+
 //Instantiates DroneDeploy API and gets object information
 function loadApi(){
 
 	console.log("calling testApi") //log message (remove laterg)
-
 	//instantiating dronedeploy api
 	new DroneDeploy({version: 1}).then(function(api){
   	console.log('DroneDeploy Api: ', api);
 
   	api.Plans.getCurrentlyViewed().then(function(plan){
-  		var zoom =16;
+  		var zoom =17;
+  		console.log("plan.then", plan)
   		api.Tiles.get({planId: plan.id, layerName: 'ortho' || 'dem', zoom: zoom}).then(function(tileInformation)
   		{
-  			console.log("tileInformation", tileInformation);
+  			console.log("tileInformation template", tileInformation.template);
+  			const tiles = getTilesFromGeometry(plan.geometry, tileInformation.template, zoom);
+  			console.log("number of tiles: ",tiles.length)
+  			for (x in tiles){
+  			console.log("tile ", tiles[x]);
+  		}
+  			
+  		//createPDF(tiles);			
   		});
   		
 
@@ -33,6 +88,16 @@ function loadApi(){
   		console.log("Plan.subscribe", plan);
   	});
 });
+
+}
+
+function createPDF(tiles)
+{
+	console.log("saving pdf", tiles)
+	var doc = new jsPDF('p');
+
+	doc.addImage(imgData, "PNG", 15, 40, 180, 160);
+  	doc.save("test.pdf");
 
 }
 
