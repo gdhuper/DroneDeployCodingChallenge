204879628b5f3078bbb17c0597825506de6bdb98
Gurpreet Singh <gdhuper@gmail.com>
2017-04-12 19:49:27 -0700

    reformatting

--- js/app.js
+++ js/app.js
@@ -20,7 +20,6 @@ var loadApp = function() {
         function lat2tile(lat, zoom) {
             return (Math.floor((1 - Math.log(Math.tan(lat * Math.PI / 180) + 1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom)));
         }
-
         function replaceInTemplate(point) {
             return template.replace('{z}', point.z)
                 .replace('{x}', point.x)
